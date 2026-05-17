36810f926fb58bc6ba7872ddd8a0b7c6b661516b
manyalalhall <manyasinghlalhall@gmail.com>
2026-05-17 18:42:50 +0530

    api test

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
